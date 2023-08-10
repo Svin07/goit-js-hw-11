56abdd95436eba52944012219bd19799c9a80374
Svin07 <svin07@ukr.net>
2023-08-10 11:03:04 +0300

    commit_4

--- src/index.js
+++ src/index.js
@@ -5,42 +5,54 @@ import axios from "axios";
 
 axios.defaults.headers.common["x-api-key"] = "38721058-dcc1021070edf740dd0c7c82a";
 
-import JSONPlaceholderAPI from './jsplaceholder-api';
-
+// import JSONPlaceholderAPI from './jsplaceholder-api';
+BASE_URL = "https://pixabay.com/api/";
+API_KEY = "x-api-key";
 
 const form = document.querySelector('#search-form');
 const list = document.querySelector('.js-list');
 const downloadBtn = document.querySelector('.load-more');
+let searchQuery = '';
 
-const JSONPlaceholderAPI = new JSONPlaceholderAPI();
+// const JSONPlaceholderAPI = new JSONPlaceholderAPI();
 
 form.addEventListener('submit', hendlerSubmit);
 downloadBtn.addEventListener('click', hendlerBtn);
 
+console.log(searchQuery);
 
 
-console.log(JSONPlaceholderAPI);
 
 function hendlerSubmit(evt) {
     evt.preventDefault();
 
-    JSONPlaceholderAPI.query = evt.currentTarget.elements.searchQuery.value;
-    JSONPlaceholderAPI.getPhoto();
+    searchQuery = evt.currentTarget.elements.searchQuery.value;
+    // JSONPlaceholderAPI.getPhoto();
   
     
-  //  getPhoto(searchQuery).then(data => (list.innerHTML = createMarkup(data.hits))).catch(err => console.log(err))
+   getPhoto(searchQuery).then(data => (list.innerHTML = createMarkup(data.hits))).catch(err => console.log(err))
     
     
 
    
 }
 
-
+console.log(searchQuery);
 
 
 
 // https://pixabay.com/api/?key=38721058-dcc1021070edf740dd0c7c82a&q=yellow+flower&image_type=photo&orientation=horizontal&safesearch=true&per_page=40
 
+async function getPhoto() {
+  let page = 1;
+        const resp = await fetch(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
+        if (!resp.ok) {
+            throw new Error(resp.statusText);
+        }
+        return resp.json();
+        
+         
+    }
 
 
 function createMarkup(arr) {
--- src/jsplaceholder-api.js
+++ src/jsplaceholder-api.js
@@ -10,7 +10,8 @@ export default class JSONPlaceholderAPI {
     }
     
     async getPhoto() {
-  
+        BASE_URL = "https://pixabay.com/api/";
+        API_KEY = "38721058-dcc1021070edf740dd0c7c82a";
         const resp = await fetch(`${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40page=${this.page}`);
         if (!resp.ok) {
             throw new Error(resp.statusText);
